cf74affbb8a5a1f4327f92e4af5ee7fe93fd980f
15874951564@163.com <zhaobaolong1>
2018-11-18 21:28:35 +0800

    第三次提交到github

--- microservice-eureka-7001/src/main/java/com/zbl/EurekaServer7001App.java
+++ microservice-eureka-7001/src/main/java/com/zbl/EurekaServer7001App.java
@@ -1,15 +1,13 @@
 package com.zbl;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class DeptProvider8001App {
-
+@EnableEurekaServer
+public class EurekaServer7001App {
 	public static void main(String[] args) {
-		SpringApplication.run(DeptProvider8001App.class,args);
+		SpringApplication.run(EurekaServer7001App.class,args);
 	}
 }
